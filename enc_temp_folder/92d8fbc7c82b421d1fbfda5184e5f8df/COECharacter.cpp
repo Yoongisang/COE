254349b50185235951c22e081eff407f0a66ecec
Yoongisang <sawsd@naver.com>
2025-07-16 04:02:15 +0900

    Paragon

---
+++ enc_temp_folder/92d8fbc7c82b421d1fbfda5184e5f8df/COECharacter.cpp
@@ -0,0 +1,240 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "COECharacter.h"
+#include "Engine/LocalPlayer.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Controller.h"
+#include "COEAnimInstance.h"
+#include "Kismet/GameplayStatics.h"
+
+
+//#include "EnhancedInputComponent.h"
+//#include "EnhancedInputSubsystems.h"
+//#include "InputActionValue.h"
+
+//DEFINE_LOG_CATEGORY(LogTemplateCharacter);
+
+ACOECharacter::ACOECharacter()
+{
+	
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+		
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false; //
+	bUseControllerRotationYaw = true; //컨트롤러 따라가게 바꿈
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = false; //컨트롤러 따라가게 바꿈
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+
+	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
+	// instead of recompiling to adjust them
+	GetCharacterMovement()->JumpZVelocity = 500.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->SocketOffset = FVector(200.f,80.f, 50.f);
+
+	// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = true; //컨트롤러 따라가게 바꿈
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+}
+
+void ACOECharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	//AnimInstance 캐스트
+	AnimInstance = Cast<UCOEAnimInstance>(GetMesh()->GetAnimInstance());
+}
+
+void ACOECharacter::DefaultAttack()
+{
+	//공격 상태
+	
+	//AnimInstance가 nullptr이 아니라면 DefaultAttackAnim 실행
+	if (IsValid(AnimInstance))
+	{
+		
+		AnimInstance->DefaultAttackAnim();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("Null"));
+	}
+	UE_LOG(LogTemp, Log, TEXT("DefaultAttack"));
+
+}
+
+void ACOECharacter::DoDefaultAttack()
+{
+	FHitResult HitResult;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+
+	float AttackRange = 100.f;
+	float AttackRadius = 50.f;
+	FVector StartPos = GetActorLocation();
+	FVector EndPos = GetActorLocation() + GetActorForwardVector() * AttackRange;
+
+	bool Result = GetWorld()->SweepSingleByChannel
+	(
+		HitResult,									//충돌 결과를 저장할 변수					
+		StartPos,									//시작 지점
+		EndPos,										//끝 지점
+		FQuat::Identity,							//회전 (기본 값)
+		ECC_GameTraceChannel3,						//충돌 채널 (Visibilirty)
+		FCollisionShape::MakeSphere(AttackRange),	//형태 : Sphere(구) MakeSphere(반지름)
+		Params										//충돌 쿼리 파라미터
+	);
+
+
+	
+	FVector Vec = GetActorForwardVector() * AttackRange;
+	FVector Center = GetActorLocation() + Vec * 0.5f;
+	float HalfHeight = AttackRange * 0.5f + AttackRadius;
+	FQuat Rotation = FRotationMatrix::MakeFromZ(Vec).ToQuat();
+	FColor DrawColor;
+
+	DrawColor = Result ? FColor::Green : FColor::Red;
+
+	DrawDebugCapsule(GetWorld(), Center, HalfHeight, AttackRadius, Rotation, DrawColor, false, 2.f);
+
+	if (Result && HitResult.GetActor())
+	{
+		UE_LOG(LogTemp, Log, TEXT("Hit : %s"), *HitResult.GetActor()->GetName());
+		//공격판정이 들어가면 데미지 적용
+		UGameplayStatics::ApplyDamage(HitResult.GetActor(), 10.f, GetInstigatorController(), this, nullptr);
+	}
+
+}
+
+void ACOECharacter::Fire()
+{
+	UE_LOG(LogTemp, Log, TEXT("Fire"));
+
+	if (IsValid(AnimInstance))
+	{
+		bIsShooting = true;
+
+		//AnimInstance->PlayAttackMontage();
+
+		float AttackRange = 10000.f;
+
+		FHitResult HitResult;
+
+		//auto
+		APlayerCameraManager* CameraManager = UGameplayStatics::GetPlayerCameraManager(this, 0);
+
+		FVector AimLocation = CameraManager->GetCameraLocation();
+		FVector TargetLocation = AimLocation + CameraManager->GetActorForwardVector() * AttackRange;
+		FCollisionQueryParams Params;
+		Params.AddIgnoredActor(this);
+
+		bool Result = GetWorld()->LineTraceSingleByChannel
+		(
+			OUT HitResult,
+			AimLocation,
+			TargetLocation,
+			ECollisionChannel::ECC_GameTraceChannel3,
+			Params
+
+		);
+
+		if (Result)
+		{
+			TargetLocation = HitResult.ImpactPoint;
+			DrawDebugLine(GetWorld(), AimLocation, TargetLocation, FColor::Green, true);
+			UE_LOG(LogTemp, Log, TEXT("Hit : %s"), *HitResult.GetActor()->GetName());
+
+			//공격판정이 들어가면 데미지 적용
+			UGameplayStatics::ApplyDamage(HitResult.GetActor(), 10.f, GetInstigatorController(), this, nullptr);
+		}
+		else
+		{
+			DrawDebugLine(GetWorld(), AimLocation, TargetLocation, FColor::Red, true);
+		}
+
+		FTransform SocketTransform = GetMesh()->GetSocketTransform(FName("ArrowSocket"));
+		SocketLocation = SocketTransform.GetLocation();
+		FVector DeltaVector = TargetLocation - SocketLocation;
+		SocketRotation = FRotationMatrix::MakeFromX(DeltaVector).Rotator();
+
+	}
+}
+
+void ACOECharacter::SetAiming(bool bNewAiming)
+{
+	bIsAiming = bNewAiming;
+	GetWorldTimerManager().ClearTimer(AimingInterpTimerHandle); // 이전 타이머 정리
+
+	StartSocketOffset = CameraBoom->SocketOffset;
+	TargetSocketOffset = bIsAiming
+		? FVector(300.f, 80.f, 50.f)
+		: FVector(200.f, 80.f, 50.f);
+
+	InterpAlpha = 0.f;
+	bInterpToAiming = bIsAiming;
+
+	GetWorldTimerManager().SetTimer(
+		AimingInterpTimerHandle,
+		this,
+		&ACOECharacter::UpdateAimingInterp,
+		0.01f, // 10ms 간격
+		true   // 반복 실행
+	);
+
+	//if (bIsAiming)
+	//{
+	//	// 조준 카메라로 전환
+	//	// 속도 감소 등
+	//	UE_LOG(LogTemp, Log, TEXT("IsAiming"));
+	//	CameraBoom->SocketOffset = FVector(300.f, 80.f, 50.f);
+	//	
+	//}
+	//else
+	//{
+	//	// 기본 상태 복귀
+	//	UE_LOG(LogTemp, Log, TEXT("IsNotAiming"));
+	//	CameraBoom->SocketOffset = FVector(200.f, 80.f, 50.f);
+	//}
+
+}
+
+float ACOECharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	//받은 데미지 표시
+	UE_LOG(LogTemp, Log, TEXT("Damaged : %f"), DamageAmount);
+	return 0.f;
+}
+
+void ACOECharacter::UpdateAimingInterp()
+{
+	InterpAlpha += 0.05f; // 0~1로 점점 증가 (속도 조절 가능)
+
+	FVector NewOffset = FMath::Lerp(StartSocketOffset, TargetSocketOffset, InterpAlpha);
+	CameraBoom->SocketOffset = NewOffset;
+
+	if (InterpAlpha >= 1.f)
+	{
+		CameraBoom->SocketOffset = TargetSocketOffset;
+		GetWorldTimerManager().ClearTimer(AimingInterpTimerHandle);
+	}
+}
+
+
